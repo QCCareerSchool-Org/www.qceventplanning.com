755915ef5aa79ce3d18cf64b8a1261ecefcf9f74
Dave Welsh <dave@qccareerschool.com>
2023-11-22 11:32:48 -0500

    Added CP

--- src/app/(main)/footerColumn1.tsx
+++ src/app/(main)/footerColumn1.tsx
@@ -4,6 +4,7 @@ import type { FC } from 'react';
 export const FooterColumn1: FC = () => (
   <>
     <li><Link href="/courses/event-and-wedding-planning">Event &amp; Wedding Planning</Link></li>
+    <li><Link href="/courses/corporate-event-planning">Corporate Event Planning</Link></li>
     <li><Link href="/courses/wedding-planning">Wedding Planning</Link></li>
     <li><Link href="/courses/event-decor">Event Decor</Link></li>
     <li><Link href="/courses/floral-design">Floral Design</Link></li>
