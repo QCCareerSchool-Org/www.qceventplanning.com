3d44ee6b490ed3655cf660807f2cad03fdb08d37
Dave Welsh <dave@qccareerschool.com>
2023-11-24 12:12:49 -0500

    Made shield gold

--- src/components/payment-plan-section/guarantee.tsx
+++ src/components/payment-plan-section/guarantee.tsx
@@ -5,7 +5,7 @@ import ShieldIcon from './shield.svg';
 
 export const PaymentSectionGuarantee: FC = () => (
   <div className="text-center">
-    <ShieldIcon className="mb-3" />
+    <ShieldIcon className={`${styles.gold} mb-3`} />
     <p className={`${styles.gold} small mb-3`}>21 DAY MONEY-BACK GUARANTEE</p>
     <h4 className="mb-3">Money-Back Guarantee</h4>
     <p className="lead mb-0">Following enrollment processing, you have 21 days to review course materials. If the course isn't suitable and you haven't submitted work to your tutor, contact us for a full refund.</p>
