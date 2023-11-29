d85086f34653e5348716ab362ccafb6df4eeafbc
Dave Welsh <dave@qccareerschool.com>
2023-11-29 15:00:53 -0500

    Added sizes image

--- src/app/(main)/tutors/blurb.tsx
+++ src/app/(main)/tutors/blurb.tsx
@@ -19,6 +19,6 @@ export const Blurb: FC<Props> = ({ name, subHeading, text, src, priority }) => (
       {subHeading && <p className="small mb-2 mb-xl-3">{subHeading}</p>}
       <p className={styles.text}>{text}</p>
     </div>
-    <Image src={src} alt={name} className={styles.image} priority={priority} />
+    <Image src={src} alt={name} className={styles.image} priority={priority} sizes="(max-width: 991px) 250px, (max-width: 1199px) 400px, (max-width: 1399px) 425px, 450px" />
   </div>
 );
