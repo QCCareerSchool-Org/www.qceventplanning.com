8fca07e739b326cecbb19848b80fd1c783af5e07
Dave Welsh <dave@qccareerschool.com>
2023-11-29 12:12:11 -0500

    Fixed linting error

--- src/app/(main)/thank-you/page.tsx
+++ src/app/(main)/thank-you/page.tsx
@@ -1,11 +1,20 @@
 import type { PageComponent } from '@/app/serverComponent';
 
 const ThankYouPage: PageComponent = ({ searchParams }) => {
+  const firstName = getParam(searchParams.firstName);
+  const lastName = getParam(searchParams.lastName);
+  const emailAddress = getParam(searchParams.emailAddress);
+  const emailOptIn = getParam(searchParams.emailOptIn);
+
   return (
     <>
       <section>
         <div className="container">
           <h1>Your Course Catalog</h1>
+          {firstName && <p>Thanks, {firstName}!</p>}
+          {lastName}
+          {emailAddress}
+          {emailOptIn}
         </div>
       </section>
     </>
@@ -13,3 +22,9 @@ const ThankYouPage: PageComponent = ({ searchParams }) => {
 };
 
 export default ThankYouPage;
+
+const getParam = (param: string[] | string | undefined): string | undefined => {
+  return Array.isArray(param)
+    ? param[0]
+    : param;
+};
