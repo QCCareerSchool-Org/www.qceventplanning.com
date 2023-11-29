cd9c59492ab64bedbe3ad4e0280b0ec436f4e7ed
Dave Welsh <dave@qccareerschool.com>
2023-11-29 13:07:24 -0500

    Updated contact page

--- src/app/(main)/contact-us/page.tsx
+++ src/app/(main)/contact-us/page.tsx
@@ -1,5 +1,21 @@
 import type { PageComponent } from '@/app/serverComponent';
+import { getData } from '@/lib/getData';
+import { getTelephoneNumber } from '@/lib/telephone';
 
-const ContactUsPage: PageComponent = () => null;
+const ContactUsPage: PageComponent = () => {
+  const { countryCode } = getData();
+  const telephoneNumber = getTelephoneNumber(countryCode);
+
+  return (
+    <>
+      <section>
+        <div className="container">
+          <h1>Contact Us</h1>
+          {telephoneNumber}
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default ContactUsPage;
