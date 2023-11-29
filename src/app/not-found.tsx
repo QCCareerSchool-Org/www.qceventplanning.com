0686fe1312563b2a6fea521f046026a43cecee69
Dave Welsh <dave@qccareerschool.com>
2023-11-29 17:59:50 -0500

    Update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,19 @@
+import type { Metadata } from 'next';
 import Link from 'next/link';
 
 import MainLayout from './(main)/layout';
-import styles from './not-found.module.css';
+import styles from './not-found.module.scss';
 import type { PageComponent } from './serverComponent';
+
+export const metadata: Metadata = {
+  title: 'Page Not Found - QC Event School',
+};
 
 const NotFoundPage: PageComponent = () => {
   return (
     <MainLayout>
       <div className={styles.wrapper}>
-        <h1>Not Found</h1>
+        <h1>Page Not Found</h1>
         <p>Sorry, we could not find the requested resource</p>
         <p><Link href="/">Home Page</Link></p>
       </div>
