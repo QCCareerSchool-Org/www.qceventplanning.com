133e7af858fc1509d4d923f9bba7a3bf9144a140
Dave Welsh <dave@qccareerschool.com>
2023-11-29 09:26:19 -0500

    Added scripts

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 
-import type { LayoutComponent } from '../serverComponent';
 import { Footer } from './footer';
 import { Header } from './header';
+import type { LayoutComponent } from '@/app/serverComponent';
+import { Bing } from '@/components/scripts/bing';
+import { Facebook } from '@/components/scripts/facebook';
+import { GoogleAnalytics } from '@/components/scripts/googleAnalytics';
+import { LiveChat } from '@/components/scripts/liveChat';
+import { Pardot } from '@/components/scripts/pardot';
+import { PerfectAudience } from '@/components/scripts/perfectAudience';
+import { Tiktok } from '@/components/scripts/tiktok';
+import { TrustPulse } from '@/components/scripts/trustPulse';
+import { VWO } from '@/components/scripts/vwo';
 
 export const metadata: Metadata = {
   title: 'QC Event School',
@@ -11,9 +21,18 @@ export const metadata: Metadata = {
 const MainLayout: LayoutComponent = ({ children }) => {
   return (
     <>
+      {process.env.GOOGLE_ANALYTICS_ID && <GoogleAnalytics id={process.env.GOOGLE_ANALYTICS_ID} adsId={process.env.GOOGLE_ADS_ID} />}
+      {process.env.VWO_ID && <VWO id={parseInt(process.env.VWO_ID, 10)} />}
       <Header />
       <main className="flex-shrink-0">{children}</main>
       <Footer />
+      {process.env.FACEBOOK_ID && <Facebook id={process.env.FACEBOOK_ID} />}
+      {process.env.TIKTOK_ID && <Tiktok id={process.env.TIKTOK_ID} />}
+      {process.env.PARDOT_ACCOUNT_ID && process.env.PARDOT_CAMPAIGN_ID && <Pardot accountId={process.env.PARDOT_ACCOUNT_ID} campaignId={process.env.PARDOT_CAMPAIGN_ID} />}
+      {process.env.BING_ID && <Bing id={process.env.BING_ID} />}
+      {process.env.TRUSTPULSE_ID && <TrustPulse id={parseInt(process.env.TRUSTPULSE_ID, 10)} />}
+      <LiveChat license={1056788} group={1} />
+      {process.env.PERFECT_AUDIENCE_ID && <PerfectAudience id={process.env.PERFECT_AUDIENCE_ID} />}
     </>
   );
 };
