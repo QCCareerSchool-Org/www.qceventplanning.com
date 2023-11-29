fc2df41e6dbd464558b8b4048036bd8971896d1e
Dave Welsh <dave@qccareerschool.com>
2023-11-29 09:28:54 -0500

    Moved scripts to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,16 @@
 import type { Metadata } from 'next';
 import { headers } from 'next/headers';
-import type { LayoutComponent } from './serverComponent';
 
+import type { LayoutComponent } from './serverComponent';
+import { Bing } from '@/components/scripts/bing';
+import { Facebook } from '@/components/scripts/facebook';
+import { GoogleAnalytics } from '@/components/scripts/googleAnalytics';
+import { LiveChat } from '@/components/scripts/liveChat';
+import { Pardot } from '@/components/scripts/pardot';
+import { PerfectAudience } from '@/components/scripts/perfectAudience';
+import { Tiktok } from '@/components/scripts/tiktok';
+import { TrustPulse } from '@/components/scripts/trustPulse';
+import { VWO } from '@/components/scripts/vwo';
 import type { GeoLocation } from '@/domain/geoLocation';
 import { neueHaasDisplay, neueHaasText } from '@/fonts';
 import { fetchGeoLocation } from '@/lib/fetch';
@@ -33,10 +42,21 @@ const RootLayout: LayoutComponent = async ({ children }) => {
 
   return (
     <html lang="en" className={`${neueHaasText.variable} ${neueHaasDisplay.variable} h-100`}>
+      <head>
+        {process.env.GOOGLE_ANALYTICS_ID && <GoogleAnalytics id={process.env.GOOGLE_ANALYTICS_ID} adsId={process.env.GOOGLE_ADS_ID} />}
+        {process.env.VWO_ID && <VWO id={parseInt(process.env.VWO_ID, 10)} />}
+      </head>
       <body className="d-flex flex-column h-100">
         <Provider geoLocation={geoLocation}>
           {children}
         </Provider>
+        {process.env.FACEBOOK_ID && <Facebook id={process.env.FACEBOOK_ID} />}
+        {process.env.TIKTOK_ID && <Tiktok id={process.env.TIKTOK_ID} />}
+        {process.env.PARDOT_ACCOUNT_ID && process.env.PARDOT_CAMPAIGN_ID && <Pardot accountId={process.env.PARDOT_ACCOUNT_ID} campaignId={process.env.PARDOT_CAMPAIGN_ID} />}
+        {process.env.BING_ID && <Bing id={process.env.BING_ID} />}
+        {process.env.TRUSTPULSE_ID && <TrustPulse id={parseInt(process.env.TRUSTPULSE_ID, 10)} />}
+        <LiveChat license={1056788} group={1} />
+        {process.env.PERFECT_AUDIENCE_ID && <PerfectAudience id={process.env.PERFECT_AUDIENCE_ID} />}
       </body>
     </html>
   );
