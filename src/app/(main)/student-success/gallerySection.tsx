888f65f51cc2336adcae0c4044e56b0f017179da
Dave Welsh <dave@qccareerschool.com>
2023-12-11 10:37:46 -0500

    Fixed linting error

--- src/app/(main)/student-success/gallerySection.tsx
+++ src/app/(main)/student-success/gallerySection.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-handler-names */
 'use client';
 
 import type { StaticImageData } from 'next/image';
@@ -13,12 +14,12 @@ import JeannetteSpencer01Image from './images/jeanette-spencer-01.jpg';
 import JordanMerlino01Image from './images/jordan-merlino-01.jpg';
 import LaurenSpignola01Image from './images/lauren-spignola-01.jpg';
 
-type ImageData = {
+type GalleryData = {
   src: StaticImageData;
   description: string;
 };
 
-const imageData: ImageData[] = [
+const data: GalleryData[] = [
   { src: AmyPetit01Image, description: 'A two-tiered wedding cake with white frosting and purple flowers.' },
   { src: CameshaIsaac01Image, description: 'A luxurious wedding dinner setup with tall, tropical centerpieces and golden chairs.' },
   { src: JeannetteSpencer01Image, description: 'The bride is having her makeup done before the wedding ceremony.' },
@@ -30,22 +31,14 @@ const imageData: ImageData[] = [
 
 export const GallerySection: FC = () => {
   const [ modal, setModal ] = useState(false);
-  const [ imageIndex, setImageIndex ] = useState<number>();
+  const [ index, setIndex ] = useState<number>();
 
   const handleModalHide = (): void => {
     setModal(false);
   };
 
-  const handleImage1Click = (): void => showModal(0);
-  const handleImage2Click = (): void => showModal(1);
-  const handleImage3Click = (): void => showModal(2);
-  const handleImage4Click = (): void => showModal(3);
-  const handleImage5Click = (): void => showModal(4);
-  const handleImage6Click = (): void => showModal(5);
-  const handleImage7Click = (): void => showModal(6);
-
-  const showModal = (index: number): void => {
-    setImageIndex(index);
+  const showModal = (i: number): void => {
+    setIndex(i);
     setModal(true);
   };
 
@@ -56,21 +49,21 @@ export const GallerySection: FC = () => {
         <p className="lead mb-5">A Glimpse into Student Achievements</p>
         <div className="row g-2 g-sm-3 g-md-4">
           <div className="col-4">
-            <GalleryImage onClick={handleImage1Click} src={AmyPetit01Image} alt="Amy Petit" aspectRatio={1} className="mb-2 mb-sm-3 mb-md-4" />
-            <GalleryImage onClick={handleImage2Click} src={CameshaIsaac01Image} alt="Camesha Isaac" aspectRatio={1} />
+            <GalleryImage onClick={() => showModal(0)} src={AmyPetit01Image} alt="Amy Petit" aspectRatio={1} className="mb-2 mb-sm-3 mb-md-4" />
+            <GalleryImage onClick={() => showModal(1)} src={CameshaIsaac01Image} alt="Camesha Isaac" aspectRatio={1} />
           </div>
           <div className="col-4">
-            <GalleryImage onClick={handleImage3Click} src={JeannetteSpencer01Image} alt="Jeannette Spencer" aspectRatio={1.777} className="mb-2 mb-sm-3 mb-md-4" />
-            <GalleryImage onClick={handleImage4Click} src={CameshaIsaac02Image} alt="Camesha Isaac" aspectRatio={1.777} className="mb-2 mb-sm-3 mb-md-4" />
-            <GalleryImage onClick={handleImage5Click} src={GianaLuchese01Image} alt="Giana Luchese" aspectRatio={1} />
+            <GalleryImage onClick={() => showModal(2)} src={JeannetteSpencer01Image} alt="Jeannette Spencer" aspectRatio={1.777} className="mb-2 mb-sm-3 mb-md-4" />
+            <GalleryImage onClick={() => showModal(3)} src={CameshaIsaac02Image} alt="Camesha Isaac" aspectRatio={1.777} className="mb-2 mb-sm-3 mb-md-4" />
+            <GalleryImage onClick={() => showModal(4)} src={GianaLuchese01Image} alt="Giana Luchese" aspectRatio={1} />
           </div>
           <div className="col-4">
-            <GalleryImage onClick={handleImage6Click} src={LaurenSpignola01Image} alt="Lauren Spignola" aspectRatio={1} className="mb-2 mb-sm-3 mb-md-4" />
-            <GalleryImage onClick={handleImage7Click} src={JordanMerlino01Image} alt="Jordan Merlino" aspectRatio={1} />
+            <GalleryImage onClick={() => showModal(5)} src={LaurenSpignola01Image} alt="Lauren Spignola" aspectRatio={1} className="mb-2 mb-sm-3 mb-md-4" />
+            <GalleryImage onClick={() => showModal(6)} src={JordanMerlino01Image} alt="Jordan Merlino" aspectRatio={1} />
           </div>
         </div>
       </div>
-      {typeof imageIndex !== 'undefined' && <GalleryModal src={imageData[imageIndex].src} description={imageData[imageIndex].description} show={modal} onHide={handleModalHide} />}
+      {typeof index !== 'undefined' && typeof data[index] !== 'undefined' && <GalleryModal src={data[index].src} description={data[index].description} show={modal} onHide={handleModalHide} />}
     </section>
   );
 };
