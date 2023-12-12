d90d003562115918695a6f097313221ea6bfdd51
Dave Welsh <dave@qccareerschool.com>
2023-12-12 13:13:43 -0500

    Reduced width of button on smaller screens

--- src/app/(landing)/buttonWrapper.tsx
+++ src/app/(landing)/buttonWrapper.tsx
@@ -13,27 +13,27 @@ export const ButtonWrapper: FC<PropsWithChildren> = ({ children }) => {
 
 const show = (screenWidth: number, scrollPosition: number): boolean => {
   if (screenWidth >= 1400) {
-    return scrollPosition >= 500;
+    return scrollPosition >= 590;
   }
 
   if (screenWidth >= 1200) {
-    return scrollPosition >= 500;
+    return scrollPosition >= 610;
   }
 
   if (screenWidth >= 992) {
-    return scrollPosition >= 500;
+    return scrollPosition >= 590;
   }
 
   if (screenWidth >= 768) {
-    return scrollPosition >= 500;
+    return scrollPosition >= 530;
   }
 
   if (screenWidth >= 576) {
-    return scrollPosition >= 500;
+    return scrollPosition >= 385;
   }
 
-  if (screenWidth >= 475) {
-    return scrollPosition >= 380;
+  if (screenWidth >= 400) {
+    return scrollPosition >= 420;
   }
 
   return false;
