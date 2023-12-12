972ee95c554fe34a62751955f7af3abcf27f5d79
Dave Welsh <dave@qccareerschool.com>
2023-12-12 13:00:29 -0500

    Updated animation

--- src/app/(landing)/free-catalog/industrySection/animation.tsx
+++ src/app/(landing)/free-catalog/industrySection/animation.tsx
@@ -4,7 +4,7 @@ import type { FC, PropsWithChildren } from 'react';
 import { Fade } from 'react-awesome-reveal';
 
 export const Animation: FC<PropsWithChildren> = ({ children }) => (
-  <Fade direction="up" triggerOnce>
+  <Fade direction="up" triggerOnce cascade>
     {children}
   </Fade>
 );
