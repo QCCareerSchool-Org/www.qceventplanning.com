68412679b0c1143ccc22d07732c5bda60dd79b86
Dave Welsh <dave@qccareerschool.com>
2023-12-14 11:06:10 -0500

    Added new enroll links

--- src/app/(landing)/free-specialty-course-offer/page.tsx
+++ src/app/(landing)/free-specialty-course-offer/page.tsx
@@ -15,6 +15,8 @@ import { DeadlineFunnelScript } from '@/components/deadlineFunnelScript';
 import { ILEASection } from '@/components/ileaSection';
 import { TestimonialSection } from '@/components/testimonialSection';
 
+const enrollLink = 'https://enroll.qceventplanning.com/free-specialty';
+
 const FreeSpecialtyCourseOfferPage: PageComponent = () => (
   <>
     <DeadlineFunnelScript />
@@ -34,10 +36,11 @@ const FreeSpecialtyCourseOfferPage: PageComponent = () => (
               type="foundation"
               name="Event & Wedding Planning"
               description="QC's most comprehensive and popular course! Learn about all the components necessary to plan successful events and weddings from start to finish."
-              href="/online-event-courses/event-and-wedding-planning"
+              href={`${enrollLink}?c=ep`}
               backgroundSrc={EPImage}
               certification={<CourseCardCertifcation courseCode="ep" />}
               message="Most Popular"
+              buttons="enroll"
             />
           </div>
           <div className="col-12 col-sm-10 col-lg-6 d-flex">
@@ -45,9 +48,10 @@ const FreeSpecialtyCourseOfferPage: PageComponent = () => (
               type="foundation"
               name="Corporate Event Planning"
               description="Discover how to plan and promote events such as product launches, galas, meetings, and conferences in this rapidly growing sector."
-              href="/online-event-courses/corporate-event-planning"
+              href={`${enrollLink}?c=cp`}
               backgroundSrc={CPImage}
               certification={<CourseCardCertifcation courseCode="cp" />}
+              buttons="enroll"
             />
           </div>
           <div className="col-12 col-sm-10 col-lg-6 d-flex">
@@ -55,9 +59,10 @@ const FreeSpecialtyCourseOfferPage: PageComponent = () => (
               type="foundation"
               name="Event Decor"
               description="Expand your creative skills to design an event's mood and aesthetic experience. Learn to coordinate lighting, color, and decor to suit a theme."
-              href="/online-event-courses/event-decor"
+              href={`${enrollLink}?c=ed`}
               backgroundSrc={EDImage}
               certification={<CourseCardCertifcation courseCode="ed" />}
+              buttons="enroll"
             />
           </div>
           <div className="col-12 col-sm-10 col-lg-6 d-flex">
@@ -65,9 +70,10 @@ const FreeSpecialtyCourseOfferPage: PageComponent = () => (
               type="foundation"
               name="Floral Design"
               description="Launch your own business, work for an experienced floral designer, or incorporate your floral design skills into an existing career in event planning."
-              href="/online-event-courses/floral-design"
+              href={`${enrollLink}?c=fd`}
               backgroundSrc={FDImage}
               certification={<CourseCardCertifcation courseCode="fd" />}
+              buttons="enroll"
             />
           </div>
           <div className="col-12 col-sm-10 col-lg-6 d-flex">
@@ -75,9 +81,10 @@ const FreeSpecialtyCourseOfferPage: PageComponent = () => (
               type="foundation"
               name="Wedding Planning"
               description="Exceed expectations by learning to plan unique weddings tailored to your client's wishes, considering cultural variations, budgets, timelines, and more."
-              href="/online-event-courses/wedding-planning"
+              href={`${enrollLink}?c=wp`}
               backgroundSrc={WPImage}
               certification={<CourseCardCertifcation courseCode="wp" />}
+              buttons="enroll"
             />
           </div>
           <div className="col-12 col-sm-10 col-lg-6 d-flex">
@@ -85,9 +92,10 @@ const FreeSpecialtyCourseOfferPage: PageComponent = () => (
               type="specialty"
               name={<>Luxury Wedding<span className="d-lg-none d-xl-inline"> &amp; Event</span> Planning</>}
               description="Master planning upscale events and weddings! Handle substantial budgets, network with high-end vendors, and embrace the latest trends."
-              href="/online-event-courses/luxury-wedding-and-event-planning"
+              href={`${enrollLink}?c=lw`}
               backgroundSrc={LWImage}
               certification={<CourseCardCertifcation courseCode="lw" />}
+              buttons="enroll"
             />
           </div>
           <div className="col-12 col-sm-10 col-lg-6 d-flex">
@@ -95,9 +103,10 @@ const FreeSpecialtyCourseOfferPage: PageComponent = () => (
               type="specialty"
               name="Destination Wedding Planning"
               description="Gain skills to coordinate stunning destination weddings. Network with travel agents, translators, and niche vendors for unforgettable events."
-              href="/online-event-courses/destination-wedding-planning"
+              href={`${enrollLink}?c=dw`}
               backgroundSrc={DWImage}
               certification={<CourseCardCertifcation courseCode="dw" />}
+              buttons="enroll"
             />
           </div>
           <div className="col-12 col-sm-10 col-lg-6 d-flex">
@@ -105,9 +114,10 @@ const FreeSpecialtyCourseOfferPage: PageComponent = () => (
               type="specialty"
               name="Accelerate Your Business"
               description="Learn how to start and grow your event planning business. Work with different marketing tools to help you achieve your business goals faster."
-              href="/online-event-courses/accelerate-your-business"
+              href={`${enrollLink}?c=eb`}
               backgroundSrc={EBImage}
               certification={<CourseCardCertifcation courseCode="eb" />}
+              buttons="enroll"
             />
           </div>
         </div>
@@ -130,7 +140,7 @@ const FreeSpecialtyCourseOfferPage: PageComponent = () => (
     <BottomSection>
       <h2 className="mb-4">Get Started Today</h2>
       <p className="lead mb-4">Enroll Online and Start on Your Path to Becoming a Certified Event Planner</p>
-      <Link href="https://enroll.qceventplanning.com/free-specialty" className="btn btn-lg btn-primary">Enroll Now</Link>
+      <Link href={enrollLink} className="btn btn-lg btn-primary">Enroll Now</Link>
     </BottomSection>
   </>
 );
--- src/components/courseCard/index.tsx
+++ src/components/courseCard/index.tsx
@@ -14,9 +14,10 @@ type Props = {
   backgroundSrc: StaticImageData;
   certification: JSX.Element;
   message?: string;
+  buttons?: 'course' | 'courseAndTuition' | 'enroll';
 };
 
-export const CourseCard: FC<Props> = ({ type, name, description, href, backgroundSrc, certification, message }) => (
+export const CourseCard: FC<Props> = ({ type, name, description, href, backgroundSrc, certification, message, buttons = 'courseAndTuition' }) => (
   <div className={`${styles.courseCard} card`}>
     <div className={styles.imageWrapper}>
       <Image src={backgroundSrc} className="card-img-top" style={{ height: 'auto' }} alt="" />
@@ -31,11 +32,20 @@ export const CourseCard: FC<Props> = ({ type, name, description, href, backgroun
         <h2 className="h4">{name}</h2>
         <p className="lead">{description}</p>
       </div>
-      <div className="d-flex flex-column flex-sm-row">
-        <Link href={href} className="btn btn-lg btn-outline-medium mb-2 mb-sm-0" style={{ whiteSpace: 'nowrap' }}>Learn More</Link>
-        {/* next/link doesn't scroll to the anchor */}
-        <a href={`${href}#paymentPlans`} className="btn btn-lg btn-none" style={{ whiteSpace: 'nowrap' }}>See Tuition<span className="ms-3"><ChevronIcon /></span></a>
-      </div>
+      {(buttons === 'courseAndTuition' || buttons === 'course') && (
+        <div className="d-flex flex-column flex-sm-row">
+          <Link href={href} className="btn btn-lg btn-outline-medium mb-2 mb-sm-0" style={{ whiteSpace: 'nowrap' }}>Learn More</Link>
+          {/* next/link doesn't scroll to the anchor */}
+          {buttons === 'courseAndTuition' && (
+            <a href={`${href}#paymentPlans`} className="btn btn-lg btn-none" style={{ whiteSpace: 'nowrap' }}>See Tuition<span className="ms-3"><ChevronIcon /></span></a>
+          )}
+        </div>
+      )}
+      {buttons === 'enroll' && (
+        <div className="d-flex flex-column flex-sm-row">
+          <Link href={href} className="btn btn-lg btn-outline-medium mb-2 mb-sm-0" style={{ whiteSpace: 'nowrap' }}>Enroll Now</Link>
+        </div>
+      )}
     </div>
   </div>
 );
