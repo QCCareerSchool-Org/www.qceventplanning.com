769de29d400a691a3b9bb9d7fe541eab05cc50d4
Dave Welsh <dave@qccareerschool.com>
2024-02-02 13:27:10 -0500

    Added certification logos and fixed svg size

--- src/components/courseCardCertification.tsx
+++ src/components/courseCardCertification.tsx
@@ -5,10 +5,13 @@ import type { FC } from 'react';
 import DWS from '@/components/certifications/dws.svg';
 import ICPP from '@/components/certifications/icpp.svg';
 import IEDP from '@/components/certifications/iedp.svg';
+import IEPP from '@/components/certifications/iepp.svg';
 import IEWP from '@/components/certifications/iewp.svg';
 import IFDP from '@/components/certifications/ifdp.svg';
+import IFLP from '@/components/certifications/iflp.svg';
 import IWPP from '@/components/certifications/iwpp.svg';
 import LWES from '@/components/certifications/lwes.svg';
+import PES from '@/components/certifications/pes.svg';
 import type { CourseCode } from '@/domain/courseCode';
 import { useScreenWidth } from '@/hooks/useScreenWidth';
 
@@ -37,6 +40,8 @@ const getCertification = (courseCode: CourseCode): any => {
       return IWPP;
     case 'cp':
       return ICPP;
+    case 'ce':
+      return IEPP;
     case 'ed':
       return IEDP;
     case 'fd':
@@ -45,5 +50,9 @@ const getCertification = (courseCode: CourseCode): any => {
       return DWS;
     case 'lw':
       return LWES;
+    case 'pe':
+      return PES;
+    case 'fl':
+      return IFLP;
   }
 };
