006f39adbc66b0bf0ae08fecfc9a77537289177d
Dave Welsh <dave@qccareerschool.com>
2024-03-21 14:30:11 -0400

    Show tutors link on XL

--- src/app/(main)/mainNav.tsx
+++ src/app/(main)/mainNav.tsx
@@ -60,8 +60,8 @@ export const MainNav: FC = () => {
             </NavDropdown>
             <Link href="/free-catalog" className="nav-link" onClick={handleClick}>Free Catalog</Link>
             <Link href="/student-success" className="nav-link" onClick={handleClick}>Student Success</Link>
+            <Link href="/tutors" className="nav-link d-lg-none d-xl-block" onClick={handleClick}>Tutors</Link>
             <Link href="/about-qc" className="nav-link" onClick={handleClick}>About QC</Link>
-            <Link href="/tutors" className="nav-link d-lg-none" onClick={handleClick}>Tutors</Link>
             <div className="d-none d-lg-block ms-3"><Link href="https://enroll.qceventplanning.com" className="btn btn-navy">Enroll Now</Link></div>
           </Nav>
         </Navbar.Collapse>
