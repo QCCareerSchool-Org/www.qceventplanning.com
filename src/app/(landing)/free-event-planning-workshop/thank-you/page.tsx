87b046a5bb21853fbaf5a7cdb7b0c7316ae60f46
Dave Welsh <dave@qccareerschool.com>
2024-04-22 15:23:31 -0400

    Added success intermediate page

--- src/app/(landing)/free-event-planning-workshop/thank-you/page.tsx
+++ src/app/(landing)/free-event-planning-workshop/thank-you/page.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
 
 const testimonialIds = [ 'TE-0013', 'TE-0002', 'TE-0003', 'TE-0004', 'TE-0005', 'TE-0006' ];
 
-const WebinarThankYouPage: PageComponent = () => {
+const WebinarThankYouPage: PageComponent = ({ searchParams }) => {
   return (
     <>
       <Header logoLink buttonAlwaysVisible buttonContent={<><span className="text-primary"><CheckIcon height="16" className="me-2" style={{ position: 'relative', top: -1 }} /></span>Enroll Now</>} buttonHref="https://enroll.qceventplanning.com" />
@@ -44,7 +44,7 @@ const WebinarThankYouPage: PageComponent = () => {
         <div className="container">
           <div className="row justify-content-center g-0">
             <div className="col-12 col-md-10 col-lg-8 text-center">
-              <h1 className="mb-4">Watch Your <span className="text-uppercase">Free</span> Event Planning Workshop Now!</h1>
+              <h1 className="mb-4">{searchParams.firstName && <>{searchParams.firstName}, </>} Watch Your <span className="text-uppercase">Free</span> Event Planning Workshop Now!</h1>
               <WistiaPlayer videoId="15k0mttyr9" wrapper="wistia-player-container-1" />
             </div>
           </div>
