45c58ad052dbffda82390edea47cf19ddc10f2b7
Dave Welsh <dave@qccareerschool.com>
2024-04-22 15:29:14 -0400

    Added support for application/x-www-form-urlencoded

--- src/app/(landing)/free-event-planning-workshop/success/route.ts
+++ src/app/(landing)/free-event-planning-workshop/success/route.ts
@@ -1,9 +1,11 @@
-export async function POST(request: Request): Promise<Response> {
+import type { NextRequest } from 'next/server';
+
+export async function POST(request: NextRequest): Promise<Response> {
   const host = request.headers.get('x-forwarded-host');
   const protocol = request.headers.get('x-forwarded-proto');
   const parameters: string[] = [];
   const contentType = request.headers.get('content-type');
-  if (contentType && (contentType.startsWith('application/form-data') || contentType.startsWith('multipart/form-data'))) {
+  if (contentType && (contentType.startsWith('application/form-data') || contentType.startsWith('multipart/form-data') || contentType.startsWith('application/x-www-form-urlencoded'))) {
     const formData = await request.formData();
     if (formData) {
       for (const entry of formData.entries()) {
