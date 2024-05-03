74df049b6ac782aa167b4bbbb84d5b879cb772e2
Dave Welsh <dave@qccareerschool.com>
2024-05-03 09:35:00 -0400

    Switched to Next's Google Analytics script

--- src/components/scripts/googleAnalytics.tsx
+++ src/components/scripts/googleAnalytics.tsx
@@ -1,46 +1,21 @@
 'use client';
 
-import { usePathname, useSearchParams } from 'next/navigation';
+import { GoogleAnalytics as NextGoogleAnalytics } from '@next/third-parties/google';
 import Script from 'next/script';
 import type { FC } from 'react';
-import { useEffect } from 'react';
-
-import { gaPageview } from '@/lib/gtag';
 
 type Props = {
   id: string;
   adsId?: string;
 };
 
-export const GoogleAnalytics: FC<Props> = ({ id, adsId }) => {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
+export const GoogleAnalytics: FC<Props> = ({ id, adsId }) => (
+  <>
+    <NextGoogleAnalytics gaId={id} />
+    {adsId && <Script id="google-analytics-google-ads" dangerouslySetInnerHTML={{ __html: getScript(adsId) }} />}
+  </>
+);
 
-  useEffect(() => {
-    const url = pathname + (searchParams ? searchParams.toString() : '');
-    gaPageview(id, url);
-  }, [ pathname, searchParams, id ]);
-
-  return (
-    <>
-      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(id)}`} />
-      <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: getScript(id, adsId) }} />
-    </>
-  );
-};
-
-const getScript = (id: string, adsId?: string): string => {
-  let script = `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', \`${id.replace(/`/ug, '\\`')}\`, {
-  page_path: window.location.pathname,
-});
-`;
-  if (adsId) {
-    script += `gtag('config', \`${adsId.replace(/`/ug, '\\`')}\`, { allow_enhanced_conversions: true });\n`;
-  }
-  return script;
+const getScript = (adsId: string): string => {
+  return `gtag('config', \`${adsId.replace(/`/ug, '\\`')}\`, { allow_enhanced_conversions: true });\n`;
 };
