b0d274b6517d765a618a98c7173f70eb244a6643
Dave Welsh <dave@qccareerschool.com>
2024-06-26 13:18:50 -0400

    Extracted header class function

--- src/components/testimonialWallSection/index.tsx
+++ src/components/testimonialWallSection/index.tsx
@@ -3,8 +3,10 @@ import type { FC } from 'react';
 import { Testimonial } from '@/components/testimonial';
 import type { CourseCode } from '@/domain/courseCode';
 
+type HeaderSize = 1 | 2 | 3 | 4 | 5 | 6;
+
 type Props = {
-  h?: 1 | 2 | 3 | 4 | 5 | 6;
+  h?: HeaderSize;
   courseCodes?: CourseCode[];
   testimonialIds: string[];
   className?: string;
@@ -17,7 +19,7 @@ export const TestimonialWallSection: FC<Props> = ({ h, courseCodes, testimonialI
       {!hideHeading && (
         <div className="row justify-content-center">
           <div className="col-12 col-lg-9 col-xl-8 col-xxl-7 text-lg-center">
-            <h2 className={`mb-4 ${typeof h === 'undefined' ? '' : `h${h}`}`}>Hear From Students and Grads</h2>
+            <h2 className={`mb-4 ${headerClass(h)}`}>Hear From Students and Grads</h2>
             <p className="lead mb-5">We've helped 8000+ students and graduates start their own successful event and wedding planning business!</p>
           </div>
         </div>
@@ -32,3 +34,5 @@ export const TestimonialWallSection: FC<Props> = ({ h, courseCodes, testimonialI
     </div>
   </section>
 );
+
+const headerClass = (h?: HeaderSize): string => (typeof h === 'undefined' ? '' : `h${h}`);
