cfac3d8ae874485eff41777090f3a077ce1fa59b
Dave Welsh <dave@qccareerschool.com>
2024-08-16 11:32:28 -0400

    Handled possibly undefined livechat function

--- src/app/(main)/contact-us/chatLink.tsx
+++ src/app/(main)/contact-us/chatLink.tsx
@@ -5,7 +5,7 @@ import type { FC, MouseEventHandler } from 'react';
 export const ChatLink: FC = () => {
   const handleClick: MouseEventHandler<HTMLAnchorElement> = e => {
     e.preventDefault();
-    window.LC_API?.open_chat_window();
+    window.LC_API?.open_chat_window?.();
   };
 
   return <a onClick={handleClick} href="#">Chat with Support</a>;
