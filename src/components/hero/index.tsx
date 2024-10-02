298f379bfb6a1427892c4fe50a3d97c5bb975fa1
Owen Moogk <owenmoogk@gmail.com>
2024-10-02 21:00:00 +0200

    Update hero to use BackgroundImageComponent

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,8 +1,6 @@
 import type { StaticImageData } from 'next/image';
-import Image from 'next/image';
 import type { CSSProperties, FC, PropsWithChildren } from 'react';
 
-import styles from './index.module.css';
 import { BackgroundImage } from '../backgroundImage';
 
 type Props = {
@@ -12,13 +10,9 @@ type Props = {
 };
 
 export const Hero: FC<PropsWithChildren<Props>> = ({ src, priority, objectPosition, children }) => (
-  <div className={styles.wrapper}>
-    <Image src={src} priority={priority} placeholder="blur" className={`${styles.image} d-none d-md-block`} alt="" sizes="100vw" />
-    <div className={styles.mobileImageWrapper}>
-      <BackgroundImage src={src} />
-    </div>
-    <div className={styles.overlay} />
-    <div className={styles.content}>
+  <div>
+    <BackgroundImage src={src} priority={priority} objectPosition={objectPosition}/>
+    <div >
       {children}
     </div>
   </div>
