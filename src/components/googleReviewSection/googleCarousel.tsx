12790587ee8f4c7d9ec1a2cf4be6c466b7a83423
Owen Moogk <owenmoogk@gmail.com>
2024-10-16 18:25:08 +0200

    Add styles needed for google carousel component

--- src/components/googleReviewSection/googleCarousel.tsx
+++ src/components/googleReviewSection/googleCarousel.tsx
@@ -2,6 +2,7 @@
 
 import type { FC } from 'react';
 import { memo, useRef } from 'react';
+import type { ResponsiveType } from 'react-multi-carousel';
 import Carousel from 'react-multi-carousel';
 
 import { GoogleReview } from './googleReview';
@@ -15,6 +16,13 @@ type Props = {
   courseCode?: CourseCode;
 };
 
+const responsive: ResponsiveType = {
+  all: {
+    breakpoint: { max: 9999, min: 0 },
+    items: 1,
+  },
+};
+
 const autoPlaySpeed = 8000; // 8 seconds
 
 export const GoogleCarousel: FC<Props> = memo(({ mobile, courseCode }) => {
@@ -24,14 +32,7 @@ export const GoogleCarousel: FC<Props> = memo(({ mobile, courseCode }) => {
 
   return (
     <div ref={carouselRef}>
-      <Carousel
-        ssr responsive={{
-          all: {
-            breakpoint: { max: 9999, min: 0 },
-            items: 1,
-          },
-        }} itemClass="d-flex" infinite arrows={!mobile} showDots={mobile} autoPlay={intersected} autoPlaySpeed={autoPlaySpeed}
-      >
+      <Carousel ssr responsive={responsive} itemClass="d-flex" infinite arrows={!mobile} showDots={mobile} autoPlay={intersected} autoPlaySpeed={autoPlaySpeed}>
         {sortedReviewData.map((data, key) => <GoogleReview {...data} key={key} />)}
       </Carousel>
     </div>
