9b54285ffc084cb484fa705fe6433754a2f5351f
Owen Moogk <owenmoogk@gmail.com>
2024-10-18 18:00:50 +0200

    Fix hero component issue

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,7 +1,8 @@
 import type { StaticImageData } from 'next/image';
+import Image from 'next/image';
 import type { CSSProperties, FC, PropsWithChildren } from 'react';
 
-import { BackgroundImage } from '../backgroundImage';
+import styles from './index.module.css';
 
 type Props = {
   src: StaticImageData;
@@ -10,9 +11,13 @@ type Props = {
 };
 
 export const Hero: FC<PropsWithChildren<Props>> = ({ src, priority, objectPosition, children }) => (
-  <div>
-    <BackgroundImage src={src} priority={priority} objectPosition={objectPosition} />
-    <div>
+  <div className={styles.wrapper}>
+    <Image src={src} priority={priority} placeholder="blur" className={`${styles.image} d-none d-md-block`} alt="" sizes="100vw" />
+    <div className={styles.mobileImageWrapper}>
+      <Image src={src} priority={priority} placeholder="blur" className={styles.image} alt="" sizes="100vw" fill style={{ objectFit: 'cover', objectPosition }} />
+    </div>
+    <div className={styles.overlay} />
+    <div className={styles.content}>
       {children}
     </div>
   </div>
