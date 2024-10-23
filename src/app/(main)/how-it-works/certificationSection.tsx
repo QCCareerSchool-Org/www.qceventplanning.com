6e1186ffeca69581ed0b5ccc15dabcb0cc96b3b1
Dave Welsh <dave@qccareerschool.com>
2024-10-23 13:20:41 -0400

    Centered text on smaller screens

--- src/app/(main)/how-it-works/certificationSection.tsx
+++ src/app/(main)/how-it-works/certificationSection.tsx
@@ -14,7 +14,7 @@ export const CertificationSection: FC = () => (
         <div className="col-12 col-lg-4 text-center">
           <CertificationLogo height="200" className="mb-3" alt="International Design and Decorating Professional (IDDP) certification logo" />
         </div>
-        <div className="col-12 col-lg-8">
+        <div className="col-12 col-lg-8 text-center text-lg-start">
           <h2 className="text-white mb-3">Become a Certified Event Planning Professional with QC Event School!</h2>
           <p>Once you have successfully completed all of your assignments and your tuition has been paid in full, you'll be all set to graduate from QC Event School! Your certificate of completion will be sent straight to you so you can begin your exciting career as an event planner.</p>
           <div className="d-lg-none" style={{ height: 100 }} />
