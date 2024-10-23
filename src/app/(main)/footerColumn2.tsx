7c8a5ca8eeaf959d5ccbfc457f14c4ca243b20fe
Dave Welsh <dave@qccareerschool.com>
2024-10-23 13:16:38 -0400

    Removed some links

--- src/app/(main)/footerColumn2.tsx
+++ src/app/(main)/footerColumn2.tsx
@@ -4,10 +4,7 @@ import type { FC } from 'react';
 export const FooterColumn2: FC = () => (
   <>
     <li><Link href="/frequently-asked-questions">FAQ</Link></li>
-    <li><Link href="/student-success">Student Success</Link></li>
-    <li><Link href="/tutors">Tutors</Link></li>
-    <li><Link href="/about-qc">About QC</Link></li>
-    <li><Link href="/how-it-works">How it Works</Link></li>
+    <li><Link href="/how-it-works">How It Works</Link></li>
     <li><Link href="/your-career">Your Career</Link></li>
     <li><a href="/blog">Blog</a></li>
     <li><a href="https://studentcenter.qccareerschool.com/students" target="_blank" rel="noreferrer">Student Login</a></li>
