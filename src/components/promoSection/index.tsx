9107765234a311cf369f3ff873ff0525ac33b66a
Dave Welsh <dave@qccareerschool.com>
2024-11-27 12:18:53 -0500

    Updated promo banner

--- src/components/promoSection/index.tsx
+++ src/components/promoSection/index.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image';
 import type { FC } from 'react';
 
-import { ClickWrapper } from './clickWrapper';
-import DesktopEnds from './desktop-ends.jpg';
-import Desktop from './desktop.jpg';
-import MobileEnds from './mobile-ends.jpg';
-import Mobile from './mobile.jpg';
+import BlackFridayBadge from './bf-label.png';
+import styles from './index.module.css';
+import TagIcon from '@/components/icons/tag.svg';
 
 type Props = {
   countryCode: string;
@@ -13,7 +11,6 @@ type Props = {
 };
 
 const startDate = Date.UTC(2024, 10, 26);
-const lastChanceDate = Date.UTC(2024, 10, 29, 8); // 2024-11-29T03:00 (08:00 UTC)
 const endDate = Date.UTC(2024, 10, 30, 8); // 2024-11-30T03:00 (08:00 UTC)
 
 export const PromoSection: FC<Props> = ({ date }) => {
@@ -21,16 +18,18 @@ export const PromoSection: FC<Props> = ({ date }) => {
     return;
   }
 
-  const [ desktopSrc, mobileSrc ] = date < lastChanceDate
-    ? [ Desktop, Mobile ]
-    : [ DesktopEnds, MobileEnds ];
-
   return (
-    <section className="bg-black color-white p-0">
-      <ClickWrapper>
-        <Image src={desktopSrc} className="d-none d-sm-block img-fluid w-100" alt="" />
-        <Image src={mobileSrc} className="d-block d-sm-none img-fluid w-100" alt="" />
-      </ClickWrapper>
+    <section className="bg-black text-white">
+      <div className="container">
+        <div className="row justify-content-center g-5">
+          <div className="col-6 col-sm-5 col-md-4 col-lg-3"><Image src={BlackFridayBadge} alt="Exclusive Black Friday savings" className="img-fluid w-100" /></div>
+          <div className="col-12 col-lg-8 col-xl-7 text-center text-lg-start">
+            <h2 className="h3 mb-4">Master Event Planning with Our Black Friday Triple-Course Offer</h2>
+            <p className={`lead mb-4 ${styles.muted}`}>Enroll in a foundation course and receive two specialty courses FREE! Gain expert guidance, industry-recognized certifications, and business training to launch your career.</p>
+            <a href="https://enroll.qceventplanning.com" className="btn btn-danger"><span className="text-black me-2" style={{ marginLeft: -3 }}><TagIcon height="16" /></span>Get Started Today</a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
