7914e7f28cb9085e28595cd62a3d7596841d19d4
Dave Welsh <dave@qccareerschool.com>
2024-12-12 10:26:39 -0500

    Added Baidu and yandex rules

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,16 +2,26 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      disallow: [
-        '/wp-admin/',
-        '/blog/wp-admin/',
-        '/*?*playAgain=',
-      ],
-      allow: '/blog/wp-admin/admin-ajax.php',
-      crawlDelay: 10,
-    },
+    rules: [
+      {
+        userAgent: 'Baiduspider',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Yandex',
+        disallow: '/',
+      },
+      {
+        userAgent: '*',
+        disallow: [
+          '/wp-admin/',
+          '/blog/wp-admin/',
+          '/*?*playAgain=',
+        ],
+        allow: '/blog/wp-admin/admin-ajax.php',
+        crawlDelay: 10,
+      },
+    ],
     sitemap: 'https://www.qceventplanning.com/sitemap_index.xml',
   };
 }
