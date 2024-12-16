8e028f82b5e4f53f6ab4de86d00a596882528c30
Dave Welsh <dave@qccareerschool.com>
2024-12-16 10:38:44 -0500

    Added schema.org metadata

--- src/app/(main)/frequently-asked-questions/question.tsx
+++ src/app/(main)/frequently-asked-questions/question.tsx
@@ -6,8 +6,14 @@ type Props = {
 
 export const Question: FC<PropsWithChildren<Props>> = ({ heading, children }) => (
   <>
-    <h3 className="h6">{heading}</h3>
-    {children}
+    <div itemScope itemProp="mainEntity" itemType="https://schema.org/Question">
+      <h3 className="h6" itemType="https://schema.org/Question">{heading}</h3>
+      <div itemScope itemProp="acceptedAnswer" itemType="https://schema.org/Answer">
+        <div itemProp="text">
+          {children}
+        </div>
+      </div>
+    </div>
     <hr />
   </>
 );
