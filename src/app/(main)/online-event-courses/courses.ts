99f544ba9729a7ca3bd5cce8c7dce7f6d521f8b3
Dave Welsh <dave@qccareerschool.com>
2025-04-03 13:38:18 -0400

    Added rating scale

--- src/app/(main)/online-event-courses/courses.ts
+++ src/app/(main)/online-event-courses/courses.ts
@@ -1,67 +1,27 @@
 import type { Course, WithContext } from 'schema-dts';
-import type { CourseCode } from '@/domain/courseCode';
+import { type CourseCode, getCourseName } from '@/domain/courseCode';
 
-type CourseData = {
-  name: string;
-  description: string;
-};
-
-const courses: Record<CourseCode, CourseData> = {
-  ep: {
-    name: 'Event & Wedding Planning',
-    description: '...',
-  },
-  wp: {
-    name: 'Wedding Planning',
-    description: '...',
-  },
-  ce: {
-    name: 'Event Planning',
-    description: '...',
-  },
-  cp: {
-    name: 'Corporate Event Planning',
-    description: '...',
-  },
-  lw: {
-    name: 'Luxury Wedding Planning',
-    description: '...',
-  },
-  dw: {
-    name: 'Destination Wedding Planning',
-    description: '...',
-  },
-  ed: {
-    name: 'Event Decor',
-    description: '...',
-  },
-  eb: {
-    name: 'Accelerate Your Business',
-    description: 'QC\'s Accelerate Your Business course provides students with the most comprehensive training available to grow your event planning business.',
-  },
-  fd: {
-    name: 'Floral Design',
-    description: '...',
-  },
-  fl: {
-    name: 'Festivals and Live Events',
-    description: '...',
-  },
-  pe: {
-    name: 'Promotional Event Planning',
-    description: '...',
-  },
-  ve: {
-    name: 'Virtual Event Planning',
-    description: '...',
-  },
+const descriptions: Record<CourseCode, string> = {
+  ep: '...',
+  wp: '...',
+  ce: '...',
+  cp: '...',
+  lw: '...',
+  dw: '...',
+  ed: '...',
+  eb: 'QC\'s Accelerate Your Business course provides students with the most comprehensive training available to grow your event planning business.',
+  fd: '...',
+  fl: '...',
+  pe: '...',
+  ve: '...',
 };
 
 export const getCourseJsonLD = (courseCode: CourseCode): WithContext<Course> => {
   return {
     '@context': 'https://schema.org',
     '@type': 'Course',
-    ...courses[courseCode],
+    'name': getCourseName(courseCode),
+    'description': descriptions[courseCode],
     'provider': {
       '@type': 'Organization',
       'name': 'QC Event School',
--- src/components/testimonial/index.tsx
+++ src/components/testimonial/index.tsx
@@ -70,6 +70,8 @@ export const Testimonial: FC<Props> = memo(({ id, courseCodes }) => {
         )}
       <span itemProp="reviewRating" itemScope itemType="https://schema.org/Rating">
         <meta itemProp="ratingValue" content={testimonial.stars.toString()} />
+        <meta itemProp="worstRating" content="0" />
+        <meta itemProp="bestRating" content="5" />
       </span>
       <div className={styles.stars}>{Array(5).fill(null).map((_, i) => <Star key={i} filled={i < testimonial.stars} />)}</div>
       <div>
