d6512f9bcf303fc1c10273b7b6cc1f42b952e8c0
Dave Welsh <dave@qccareerschool.com>
2025-04-23 09:24:15 -0400

    Added award of excellence

---
+++ src/app/(main)/award/[submissionId]/suggestedText.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import type { FC, MouseEventHandler } from 'react';
+import { useEffect, useState } from 'react';
+
+type Params = {
+  text: string;
+};
+
+export const SuggestedText: FC<Params> = ({ text }) => {
+  const [ clicks, setClicks ] = useState(0);
+
+  useEffect(() => {
+    if (!clicks) {
+      return;
+    }
+    const id = setTimeout(() => setClicks(0), 1000);
+    return () => clearTimeout(id);
+  }, [ clicks ]);
+
+  const handleTextAreaClick: MouseEventHandler<HTMLTextAreaElement> = () => {
+    void navigator.clipboard.writeText(text).then(() => {
+      setClicks(c => (c === Number.MAX_SAFE_INTEGER ? 1 : c + 1));
+    });
+  };
+
+  return (
+    <>
+      <textarea onClick={handleTextAreaClick} className="form-control" readOnly rows={4} style={{ resize: 'none', marginBottom: '0.25rem', borderRadius: '0.5rem' }} value={text} />
+      <div style={{ height: '1rem' }}>
+        {clicks > 0 && <small style={{ textTransform: 'uppercase' }}>Text Copied!</small>}
+      </div>
+    </>
+  );
+};
---
+++ src/components/share/threads/index.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import type { FC, MouseEventHandler } from 'react';
+
+import styles from './index.module.css';
+import globalStyles from '../global.module.css';
+
+type Props = {
+  text: string;
+};
+
+export const ThreadsShare: FC<Props> = ({ text }) => {
+  const handleClick: MouseEventHandler<HTMLAnchorElement> = e => {
+    e.preventDefault();
+    const target = e.target as HTMLAnchorElement;
+    const href = target.href;
+    window.open(href, 'Share on Threads', 'width=640, height=536');
+  };
+
+  return <a onClick={handleClick} href={`https://www.threads.net/intent/post?text=${encodeURIComponent(text)}`} target="_blank" rel="noreferrer" className={`${globalStyles.button} ${styles.button}`}>Share on Threads</a>;
+};
