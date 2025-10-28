324f2f590e46feea392975242218458685eb8033
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-10-28 11:08:36 -0400

    Remove on-scroll header button

--- src/app/(landing)/(thank-you)/thank-you-course-catalog/page.tsx
+++ src/app/(landing)/(thank-you)/thank-you-course-catalog/page.tsx
@@ -6,7 +6,6 @@ import { Header } from '../../header';
 import { CurrentPromotion } from '../_components/currentPromotion';
 import { ThankYouSection } from '.';
 import type { PageComponent } from '@/app/serverComponent';
-import DownloadIcon from '@/components/download.svg';
 import { GoogleReviewSection } from '@/components/googleReviewSection';
 import HeroLgImage from '@/components/homeHeroImage/hero-large.jpg';
 import HeroSmImage from '@/components/homeHeroImage/hero-small.jpg';
@@ -58,7 +57,7 @@ const ThankYouCourseCatalogPage: PageComponent = async ({ searchParams }) => {
         leadId={leadId}
         conversionId="AW-1071836607/9wB_CNvknggQv9uL_wM"
       />
-      <Header logoLink buttonContent={<><span className="text-light"><DownloadIcon height="14" className="me-2" style={{ position: 'relative', top: -1 }} /></span><span className="d-none d-sm-inline">Get Your Free </span>Catalog</>} showBanner />
+      <Header logoLink showBanner />
       <ThankYouSection heroSrc={HeroLgImage} mobileHeroSrc={HeroSmImage} emailAddress={emailAddress} />
       <CurrentPromotion date={date} countryCode={countryCode} />
       <GoogleReviewSection className="bg-light" />
