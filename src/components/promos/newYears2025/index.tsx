e27a946e9c7bae710f35bd5232f4aa139b54f6bb
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-12-18 16:33:42 -0500

    Update promo section images

--- src/components/promos/newYears2025/index.tsx
+++ src/components/promos/newYears2025/index.tsx
@@ -1,16 +1,13 @@
 import Image from 'next/image';
 import type { FC } from 'react';
 
-import HeroImageUK from './desktop-uk.jpg';
 import HeroImageDefault from './desktop.jpg';
-import { gbpCountry } from '@/domain/currency';
 
 interface Props {
   countryCode: string;
 }
 
 export const NewYears2025: FC<Props> = ({ countryCode }) => {
-  const heroImageSrc = gbpCountry(countryCode) ? HeroImageUK : HeroImageDefault;
 
   return (
     <section>
@@ -19,7 +16,7 @@ export const NewYears2025: FC<Props> = ({ countryCode }) => {
           <div className="col-12 col-md-10 col-lg-8 col-xl-7">
             <h2 className="mb-3">Enroll Today and Get 2 Free Courses!</h2>
             <div className="mb-4">
-              <a href="https://enroll.qceventplanning.com"><Image src={heroImageSrc} alt="" className="img-fluid" /></a>
+              <a href="https://enroll.qceventplanning.com"><Image src={HeroImageDefault} alt="" className="img-fluid" /></a>
             </div>
             <a href="https://enroll.qceventplanning.com"><button className="btn btn-primary btn-lg">CLAIM MY FREE COURSES</button></a>
           </div>
