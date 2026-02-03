3fd312795be74c4244249ddf639db7d9efdedd5c
Dave Welsh <dave@qccareerschool.com>
2026-02-03 07:29:30 -0500

    Fixed server post

--- src/app/(landing)/(thank-you)/download-floral-design-course-catalog/page.tsx
+++ src/app/(landing)/(thank-you)/download-floral-design-course-catalog/page.tsx
@@ -42,7 +42,7 @@ const ThankYouCourseCatalogPage: PageComponent = async props => {
 
   if (lead?.success) {
     recent = lead.value.created < date + 604_800_000; // 7 days
-    if (!recent) {
+    if (recent) {
       try {
         await fbPostLead(lead.value.leadId, new Date(lead.value.created), lead.value.emailAddress, lead.value.firstName, lead.value.lastName, lead.value.countryCode, data.url, lead.value.ip ?? data.serverIp, data.userAgent, data.fbc, data.fbp);
       } catch (err) {
