e929de64fd4971196c4147b375a3076805699a17
Dave Welsh <dave@qccareerschool.com>
2026-02-03 16:16:01 -0500

    Added bing data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ const RootLayout: LayoutComponent = async ({ children }) => {
         {process.env.BREVO_CLIENT_KEY && <Brevo clientKey={process.env.BREVO_CLIENT_KEY} />}
         {process.env.FACEBOOK_ID && <Facebook id={process.env.FACEBOOK_ID} userValues={userValues} />}
         {process.env.TIKTOK_ID && <Tiktok id={process.env.TIKTOK_ID} />}
-        {process.env.BING_ID && <Bing id={process.env.BING_ID} />}
+        {process.env.BING_ID && <Bing id={process.env.BING_ID} userValues={userValues} />}
       </head>
       <body className="d-flex flex-column">
         <Provider userValues={userValues} serverIp={serverIp}>
