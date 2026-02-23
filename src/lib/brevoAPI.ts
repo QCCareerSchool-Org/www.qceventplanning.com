b23fb92707bf8a1912e8364607e7cf653f6397ae
Dave Welsh <dave@qccareerschool.com>
2026-02-23 13:01:36 -0500

    Updated brevo

--- src/lib/brevoAPI.ts
+++ src/lib/brevoAPI.ts
@@ -1,8 +1,10 @@
 import 'server-only';
 
-import * as brevo from '@getbrevo/brevo';
+import { BrevoClient } from '@getbrevo/brevo';
 
-const brevoApiKey = process.env.BREVO_API_KEY ?? '';
+const apiKey = process.env.BREVO_API_KEY ?? '';
+
+const brevo = new BrevoClient({ apiKey });
 
 interface Attributes {
   STATUS_EVENT_LEAD?: boolean;
@@ -11,16 +13,14 @@ interface Attributes {
 
 export const createBrevoContact = async (emailAddress: string, firstName?: string, lastName?: string, countryCode?: string, provinceCode?: string | null, attributes?: Attributes, listIds?: number[]): Promise<boolean> => {
   console.log('adding brevo contact', emailAddress);
-  const contactsApi = new brevo.ContactsApi();
-  contactsApi.setApiKey(brevo.ContactsApiApiKeys.apiKey, brevoApiKey);
 
-  const body: brevo.CreateContact = {
+  const body = {
     email: emailAddress,
     listIds,
     updateEnabled: true,
     attributes: {
       ...attributes,
-    } as brevo.CreateContact['attributes'],
+    },
   };
 
   if (typeof firstName !== 'undefined') {
@@ -36,18 +36,16 @@ export const createBrevoContact = async (emailAddress: string, firstName?: strin
     (body.attributes as Record<string, unknown>).PROVINCE_CODE = provinceCode === null ? '' : provinceCode.toLocaleUpperCase();
   }
 
-  const createContactResult = await contactsApi.createContact(body);
+  const createContactResponse = await brevo.contacts.createContact(body);
 
-  return createContactResult.response.complete;
+  return typeof createContactResponse.id !== 'undefined';
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const sendBrevoEmail = async (templateId: number, emailAddress: string, firstName?: string, lastName?: string): Promise<void> => {
   console.log('sending brevo email', emailAddress);
-  const transactionalEmailsApi = new brevo.TransactionalEmailsApi();
-  transactionalEmailsApi.setApiKey(brevo.TransactionalEmailsApiApiKeys.apiKey, brevoApiKey);
 
-  await transactionalEmailsApi.sendTransacEmail({
+  await brevo.transactionalEmails.sendTransacEmail({
     to: firstName ? [ { email: emailAddress, name: firstName } ] : [ { email: emailAddress } ],
     templateId,
     // params: { name: firstName, surname: lastName },
