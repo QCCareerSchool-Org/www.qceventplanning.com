b74abccff161613f0de03dbe553e5ac89da588a5
Dave Welsh <dave@qccareerschool.com>
2026-03-17 14:56:19 -0400

    Added promo banner

--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -2,20 +2,22 @@ import type { FC } from 'react';
 
 import { PromoBanner } from './promoBanner';
 import { MainNav } from '../../app/(main)/mainNav';
-import { mar04 } from '@/periods';
+import { gbpCountry } from '@/domain/currency';
+import { mar18 } from '@/periods';
 
 interface Props {
   countryCode: string;
   date: number;
 }
 
-export const Header: FC<Props> = ({ date }) => {
+export const Header: FC<Props> = ({ countryCode, date }) => {
+  const discount = gbpCountry(countryCode) ? '£100' : '$100';
   return (
     <header className={`flex-shrink-0`} style={{ position: 'sticky', top: 0, zIndex: 1020, width: '100%' }}>
-      {mar04.contains(date)
+      {mar18.contains(date)
         ? (
-          <PromoBanner date={date} period={mar04.toDTO()}>
-            <span className="d-none d-lg-inline">Don't Miss Out—</span>Get 2 specialty courses <strong>free</strong>
+          <PromoBanner date={date} period={mar18.toDTO()}>
+            <span className="d-none d-lg-inline">Don't Miss Out—</span>Get 2 free specialty courses <strong>+ {discount} OFF</strong>
           </PromoBanner>
         )
         : null
