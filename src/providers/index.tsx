721bfb4af86c6bfb46d9b56f2fff0224d6bdaf9a
Dave Welsh <dave@qccareerschool.com>
2026-04-13 10:30:35 -0400

    Updated cdn link

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,11 +2,11 @@ import type { FC, PropsWithChildren } from 'react';
 
 import { CaptchaProvider } from './captchaProvider';
 import { IPProvider } from './ipProvider';
+import { ScreenSizeProvider } from './screenSizeProvider';
 import { ScrollPositionProvider } from './scrollPositionProvider';
 import { TaxCreditPopupProvider } from './taxCreditPopupProvider';
 import { UserValuesProvider } from './userValuesProvider';
 import type { UserValues } from '@/domain/userValues';
-import { ScreenSizeProvider } from '@/hooks/useScreenSizeContext';
 
 const reCaptchaKey = process.env.NEXT_PUBLIC_RECAPTCHA_KEY;
 
