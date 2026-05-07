9e5b188e7b596a89bfb99a524062f3b62444283f
Angela Li <angelayxli@gmail.com>
2026-05-07 17:04:53 -0400

    clean up redundancies

--- src/app/(landing)/(email-preferences)/email-preferences-no/page.tsx
+++ src/app/(landing)/(email-preferences)/email-preferences-no/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
 
-import { getThankyouData } from '../../(thank-you)';
 import { CurrentPromotion } from '../../(thank-you)/_components/currentPromotion';
 import { Header } from '../../header';
 import { EmailPreferencesNoSection } from '../_components/emailPreferencesNoSection';
@@ -9,24 +8,24 @@ import { GoogleReviewSection } from '@/components/googleReviewSection';
 import HeroLgImage from '@/components/homeHeroImage/hero-large.jpg';
 import HeroSmImage from '@/components/homeHeroImage/hero-small.jpg';
 import { ILEASection } from '@/components/ileaSection';
-import { LeadProcessing } from '@/components/leadProcessing';
-import { SetCookie } from '@/components/setCookie';
 import { SupportSection } from '@/components/supportSection';
 import { addToBrevoList, getBrevoContactId } from '@/lib/brevoAPI';
+import { getServerData } from '@/lib/getServerData';
 import type { PageComponent } from '@/serverComponent';
 
 export const metadata: Metadata = {
-  title: 'Your Course Catalog',
-  alternates: { canonical: '/thank-you-course-catalog' },
+  title: 'No problem, we’ll update your email preferences!',
+  description: 'From now on, we’ll only reach out with specific course updates or offers when you’re actively engaging with QC. Want to leave us for good? You can find the \'Unsubscribe\' link at the bottom of your last email.',
+  alternates: { canonical: '/email-preferences-no' },
   robots: { index: false },
 };
 
 const telephoneListId = 53;
+const listId = 102;
 
 const EmailPreferencesNoPage: PageComponent = async props => {
-  const { countryCode, emailAddress, lead, jwt, recent, date } = await getThankyouData(props);
+  const { countryCode, date } = await getServerData(props.searchParams);
   const searchParams = await props.searchParams;
-  const listId = 102;
   const sc = searchParams._sc;
 
   if (typeof sc === 'string') {
@@ -42,21 +41,8 @@ const EmailPreferencesNoPage: PageComponent = async props => {
 
   return (
     <>
-      {jwt && <SetCookie name="user" value={jwt} domain="qceventplanning.com" />}
-      {lead && recent && (
-        <LeadProcessing
-          emailAddress={lead.emailAddress}
-          telephoneNumber={lead.telephoneNumber}
-          city={lead.city}
-          countryCode={lead.countryCode}
-          provinceCode={lead.provinceCode}
-          firstName={lead.firstName}
-          lastName={lead.lastName}
-          leadId={lead.leadId}
-        />
-      )}
       <Header logoLink showBanner buttonAlwaysVisible buttonContent="Enroll Now" buttonHref="https://enroll.qceventplanning.com" />
-      <EmailPreferencesNoSection emailAddress={emailAddress} countryCode={countryCode} heroSrc={HeroLgImage} mobileHeroSrc={HeroSmImage} leadId={lead?.leadId} telephoneListId={alreadyPrompted ? undefined : telephoneListId} />
+      <EmailPreferencesNoSection countryCode={countryCode} heroSrc={HeroLgImage} mobileHeroSrc={HeroSmImage} telephoneListId={alreadyPrompted ? undefined : telephoneListId} />
       <CurrentPromotion date={date} countryCode={countryCode} />
       <GoogleReviewSection className="bg-light" />
       <ILEASection />
--- src/app/(landing)/(email-preferences)/email-preferences-yes/page.tsx
+++ src/app/(landing)/(email-preferences)/email-preferences-yes/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
 
-import { getThankyouData } from '../../(thank-you)';
 import { CurrentPromotion } from '../../(thank-you)/_components/currentPromotion';
 import { Header } from '../../header';
 import { EmailPreferencesYesSection } from '../_components/emailPreferencesYesSection';
@@ -9,41 +8,28 @@ import { GoogleReviewSection } from '@/components/googleReviewSection';
 import HeroLgImage from '@/components/homeHeroImage/hero-large.jpg';
 import HeroSmImage from '@/components/homeHeroImage/hero-small.jpg';
 import { ILEASection } from '@/components/ileaSection';
-import { LeadProcessing } from '@/components/leadProcessing';
-import { SetCookie } from '@/components/setCookie';
 import { SupportSection } from '@/components/supportSection';
+import { getServerData } from '@/lib/getServerData';
 import type { PageComponent } from '@/serverComponent';
 
 export const metadata: Metadata = {
-  title: 'Your Course Catalog',
-  alternates: { canonical: '/thank-you-course-catalog' },
+  title: 'You’re officially back in the loop!',
+  description: 'Thanks for updating your preferences. We’ll keep sending you design tips, student success stories, exclusive offers, and updates from QC Design School.',
+  alternates: { canonical: '/email-preferences-yes' },
   robots: { index: false },
 };
 
 const telephoneListId = 53;
 
 const EmailPreferencesYesPage: PageComponent = async props => {
-  const { countryCode, lead, jwt, recent, date } = await getThankyouData(props);
+  const { countryCode, date } = await getServerData(props.searchParams);
   const searchParams = await props.searchParams;
   const alreadyPrompted = searchParams.t;
 
   return (
     <>
-      {jwt && <SetCookie name="user" value={jwt} domain="qceventplanning.com" />}
-      {lead && recent && (
-        <LeadProcessing
-          emailAddress={lead.emailAddress}
-          telephoneNumber={lead.telephoneNumber}
-          city={lead.city}
-          countryCode={lead.countryCode}
-          provinceCode={lead.provinceCode}
-          firstName={lead.firstName}
-          lastName={lead.lastName}
-          leadId={lead.leadId}
-        />
-      )}
       <Header logoLink showBanner buttonAlwaysVisible buttonContent="Enroll Now" buttonHref="https://enroll.qceventplanning.com" />
-      <EmailPreferencesYesSection countryCode={countryCode} heroSrc={HeroLgImage} mobileHeroSrc={HeroSmImage} leadId={lead?.leadId} telephoneListId={alreadyPrompted ? undefined : telephoneListId} />
+      <EmailPreferencesYesSection countryCode={countryCode} heroSrc={HeroLgImage} mobileHeroSrc={HeroSmImage} telephoneListId={alreadyPrompted ? undefined : telephoneListId} />
       <CurrentPromotion date={date} countryCode={countryCode} />
       <GoogleReviewSection className="bg-light" />
       <ILEASection />
