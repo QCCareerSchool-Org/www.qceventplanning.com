ecbb019d70444100752851d26524fe58ab747a16
Dave Welsh <dave@qccareerschool.com>
2026-07-06 10:31:03 -0400

    Improved active campaign script

--- src/lib/safeJSON.ts
+++ src/lib/safeJSON.ts
@@ -1,3 +1,5 @@
+import { stringify } from './json';
+
 export const safeJSON = (obj: unknown): string | undefined => {
-  return (JSON.stringify(obj) as string | undefined)?.replace(/</gu, '\\u003c');
+  return stringify(obj).replace(/</gu, '\\u003c');
 };
