c12af0ebb275c224c4f0b1315c5648661e8cb256
Dave Welsh <dave@qccareerschool.com>
2026-07-16 11:34:40 -0400

    Added uservalues

--- src/components/siteLayout/index.tsx
+++ src/components/siteLayout/index.tsx
@@ -3,6 +3,7 @@ import type { FC, PropsWithChildren } from 'react';
 import { Footer } from './footer';
 import { Header } from './header';
 import { TaxCreditsModal } from '@/components/taxCreditsModal';
+import type { UserValues } from '@/domain/userValues';
 import { LiveChat } from '@/scripts/liveChat';
 
 import '@/app/bootstrap.scss';
@@ -11,15 +12,17 @@ interface Props {
   countryCode: string;
   provinceCode: string | null;
   date: number;
+  userValues?: UserValues;
 }
 
-export const SiteLayout: FC<PropsWithChildren<Props>> = ({ countryCode, provinceCode, date, children }) => {
+export const SiteLayout: FC<PropsWithChildren<Props>> = ({ countryCode, provinceCode, date, userValues, children }) => {
+
   return (
     <>
       <Header countryCode={countryCode} provinceCode={provinceCode} date={date} />
       <main className="flex-shrink-0">{children}</main>
       <Footer />
-      {process.env.LIVECHAT_LICENSE && <LiveChat group={process.env.LIVECHAT_GROUP_ID} license={process.env.LIVECHAT_LICENSE} />}
+      {process.env.LIVECHAT_LICENSE && <LiveChat group={process.env.LIVECHAT_GROUP_ID} license={process.env.LIVECHAT_LICENSE} userValues={userValues} />}
       <TaxCreditsModal />
     </>
   );
